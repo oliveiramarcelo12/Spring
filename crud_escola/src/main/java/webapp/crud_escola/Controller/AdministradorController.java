b56cf29bf7fd1a2d3241bc4e1c9b36c6d8d28d37
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-03-22 22:02:15 -0300

    22/03

--- crud_escola/src/main/java/webapp/crud_escola/Controller/AdministradorController.java
+++ crud_escola/src/main/java/webapp/crud_escola/Controller/AdministradorController.java
@@ -5,18 +5,24 @@ import org.springframework.stereotype.Controller;
 
 import webapp.crud_escola.Model.Administrador;
 import webapp.crud_escola.Repository.AdministradorRepository;
+import webapp.crud_escola.Repository.VerificaCadastroAdmRepository;
 
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
-public class AdministradorController {
+public class AdministradorController  {
     @Autowired
     private AdministradorRepository ar ;
+    private VerificaCadastroAdmRepository vcar;
 
     @PostMapping("/cad-adm")
     public String postCadAdm (Administrador adm) {
-        ar.save(adm);
+     String cpfVerificar = vcar.findByCpf(adm.getCpf()).getCpf();
+        if (cpfVerificar.equals(adm.getCpf())) {
+            ar.save(adm); 
+        }
+      
         return "adm/login-adm";
     }
     
