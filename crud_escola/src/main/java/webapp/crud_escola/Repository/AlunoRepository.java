e61a1fe3052f026d9c54e40deeb6fa5c842fa4b1
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-04-01 20:58:31 -0300

    01/04

--- crud_escola/src/main/java/webapp/crud_escola/Repository/AlunoRepository.java
+++ crud_escola/src/main/java/webapp/crud_escola/Repository/AlunoRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import webapp.crud_escola.Model.Aluno;
 
-public interface AlunoRepository extends CrudRepository<Aluno,String>  {
-    
+public interface AlunoRepository extends CrudRepository<Aluno, String> {
+    Aluno findByCpf(String cpf);
+
+    Aluno findBySenha(String senha);
+
 }
